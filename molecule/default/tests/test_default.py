4cf6bef832bfb52943689b610504a7c26a71ed09
Ville Harmaala <harppi@harppi.fi>
2021-09-08 20:28:52 +0300

    Test if duplicity dirs exist and their permissions

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -5,7 +5,8 @@ import testinfra.utils.ansible_runner
 testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
-@pytest.mark.parametrize("name", [
+# Test if apt installed the required packages
+@pytest.mark.parametrize("package", [
     ("python3-venv"),
     ("python3-pip"),
     ("gettext"),
@@ -13,41 +14,60 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     ("haveged"),
 ])
 
-def test_apt_prerequisites(host, name):
-    pkg = host.package(name)
+def test_apt_prerequisites(host, package):
+    pkg = host.package(package)
     assert pkg.is_installed
 
-@pytest.mark.parametrize("name", [
+
+# Test if pip3 installed the required packages
+@pytest.mark.parametrize("package", [
     ("dropbox"),
     ("duplicity"),
 ])
 
-def test_pip_prerequisites(host, name):
+def test_pip_prerequisites(host, package):
     pkgs = host.pip.get_packages(pip_path='/usr/bin/pip3')
-    assert name in pkgs
+    assert package in pkgs
 
-# Ensure GPG keypair exists
+
+# Test if a certain GPG keypair exists
 def test_gpg_keypair(host):
     cmd = host.run("gpg --list-keys | grep root@root.com")
     assert cmd.succeeded == True
 
-@pytest.mark.parametrize("gpg_dir_permissions", [
+
+# Test if GPG directories exist and if their permissions are correct
+@pytest.mark.parametrize("gpg_dir", [
     ("/root/.gnupg/"),
     ("/root/.gnupg/private-keys-v1.d/"),
     ("/root/.gnupg/openpgp-revocs.d/"),
 ])
 
-def test_gpg_permissions(host, gpg_dir_permissions):
-    gpg = host.file(gpg_dir_permissions)
-    assert gpg.exists
-    assert gpg.user == "root"
-    assert gpg.group == "root"
-    assert gpg.mode == 0o700
+def test_gpg_permissions(host, gpg_dir):
+    gpg_dir = host.file(gpg_dir)
+    assert gpg_dir.exists
+    assert gpg_dir.user == "root"
+    assert gpg_dir.group == "root"
+    assert gpg_dir.mode == 0o700
 
-# Ensure duplicity directory exists, permissions
 
-# Ensure env vars file exists, permissions
+# Test if duplicity directories exist and if their permissions are correct
+@pytest.mark.parametrize("duplicity_dirs", [
+    ("/.duplicity"),
+    ("/.duplicity/.backup.sh"),
+    ("/.duplicity/.restore.sh"),
+    ("/var/log/duplicity")
+])
 
-# Ensure log directory exists, permissions
+def test_duplicity_permissions(host, duplicity_dirs):
+    duplicity_dir = host.file(duplicity_dirs)
+    assert duplicity_dir.exists
+    assert duplicity_dir.user == "root"
+    assert duplicity_dir.group == "root"
+    assert duplicity_dir.mode == 0o700
 
-# Ensure backup and restoration scripts exist, permissions
+    env_var_conf = host.file("/.duplicity/.env_variables.conf")
+    assert env_var_conf.exists
+    assert env_var_conf.user == "root"
+    assert env_var_conf.group == "root"
+    assert env_var_conf.mode == 0o600
